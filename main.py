ee1dfad8ce9b18707f98ac48d61c503f75216ba5
JadaE <jadaebong@gmail.com>
2020-03-16 23:48:40 -0500

    got stuck on the dictionaries but here's tonights work
    could make a sketch 400 but wanted to neaten it with a dictionary of lists. might try to fast forward with the images next.

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 #make it here
 import random
+count = 0
+dictOfBoards = dict.fromkeys(range(5),[])
+print(dictOfBoards)
+while count!=1:
+    #dictOfBoards[count]
+    #all possible cards
+    cards = ["El gallo","El diablito","La dama","El catrín",
+            "El paraguas","La sirena","La escalera","La botella",
+            "El barril","El árbol","El melón","El valiente",
+            "El gorrito","La muerte","La pera","La bandera","El bandolón",
+            "El violoncello","La garza","El pájaro","La mano","La bota",
+            "La luna","El cotorro","El borracho","El Rowdy","El corazón",
+            "La sandía","El tambor","El camarón","Las jaras","El músico",
+            "La araña","El soldado","La estrella","El cazo","El mundo",
+            "El Roadrunner","El nopal","El alacrán","La rosa","La calavera",
+            "La campana","El cantarito","El venado","El Sol","La corona",
+            "La chalupa","El pino","El pescado","La palma","La maceta",
+            "El arpa","La rana"]
+            #change? : el negrito(Rowdy), el apache(Roadrunner), el borracho?
 
-#all possible cards
-cards = ["El gallo","El diablito","La dama","El catrín",
-        "El paraguas","La sirena","La escalera","La botella",
-        "El barril","El árbol","El melón","El valiente",
-        "El gorrito","La muerte","La pera","La bandera","El bandolón",
-        "El violoncello","La garza","El pájaro","La mano","La bota",
-        "La luna","El cotorro","El borracho","El Rowdy","El corazón",
-        "La sandía","El tambor","El camarón","Las jaras","El músico",
-        "La araña","El soldado","La estrella","El cazo","El mundo",
-        "El Roadrunner","El nopal","El alacrán","La rosa","La calavera",
-        "La campana","El cantarito","El venado","El Sol","La corona",
-        "La chalupa","El pino","El pescado","La palma","La maceta",
-        "El arpa","La rana"]
-        #change? : el negrito(Rowdy), el apache(Roadrunner), el borracho?
+    #slot = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    #Two options: Randomize and check all other card combos if it already happened 
+    #OR go through in order until 400 but everyones first couple will be same - this wont work bc everyone could get same bingo
 
-slot = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-#Two options: Randomize and check all other card combos if it already happened 
-#OR go through in order until 400 but everyones first couple will be same - this wont work bc everyone could get same bingo
-for x in slot:
-    new = random.choice(cards)
-    #print (new)
-    for y in cards:
-        if new == y:
-            cards.remove(new)
-    slot[x] = new
-print(slot)
-print(cards)
+    slot = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    for x in slot:
+        newS = random.choice(cards)
+        cards.remove(newS)
+        #for y in cards:
+        #    if newS == y:
+        #        cards.remove(newS)
+        slot[x] = newS
+    #print(slot)
+    dictOfBoards[count].update(slot)
+    dictOfBoards[count].append('*********************')
+    count = count +1
 
-
+print(str(dictOfBoards))
