bce9e234e22a9b002bf26b9163afa88cdcb9a907
JadaE <jadaebong@gmail.com>
2020-03-21 17:36:35 -0500

    reorganizing
    rid of dictionaries, added folders, edited readme, included necessary imports

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #make it here
-import random
+import random, os
+from fpdf import FPDF
 count = 0
-dictOfBoards = dict.fromkeys(range(5),[])
-print(dictOfBoards)
-while count!=1:
-    #dictOfBoards[count]
+#dictOfBoards = dict.fromkeys(range(5),[])
+while count!=5:
     #all possible cards
     cards = ["El gallo","El diablito","La dama","El catrín",
             "El paraguas","La sirena","La escalera","La botella",
             "El barril","El árbol","El melón","El valiente",
             "El gorrito","La muerte","La pera","La bandera","El bandolón",
             "El violoncello","La garza","El pájaro","La mano","La bota",
-            "La luna","El cotorro","El borracho","El Rowdy","El corazón",
+            "La luna","El cotorro","El HEB Partner","El Rowdy","El corazón",
             "La sandía","El tambor","El camarón","Las jaras","El músico",
             "La araña","El soldado","La estrella","El cazo","El mundo",
             "El Roadrunner","El nopal","El alacrán","La rosa","La calavera",
@@ -20,7 +19,7 @@ while count!=1:
             "El arpa","La rana"]
             #change? : el negrito(Rowdy), el apache(Roadrunner), el borracho?
 
-    #slot = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    #slot = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]s
     #Two options: Randomize and check all other card combos if it already happened 
     #OR go through in order until 400 but everyones first couple will be same - this wont work bc everyone could get same bingo
 
@@ -32,12 +31,13 @@ while count!=1:
         #    if newS == y:
         #        cards.remove(newS)
         slot[x] = newS
-    #print(slot)
-    dictOfBoards[count].update(slot)
-    dictOfBoards[count].append('*********************')
+    print(slot)
+    print()
+    #dictOfBoards[count].update(slot)
+    #dictOfBoards[count].append('*********************')
     count = count +1
 
-print(str(dictOfBoards))
+#print(str(dictOfBoards))
 #static folder holding all images, find image in folder
 #image files have to be consistant, all certain size
 #python libraries for dropping pics onto other pics
