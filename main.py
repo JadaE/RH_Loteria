307078bd854c714823976f8555b4ce63e8cfc1a5
JadaE <jadaebong@gmail.com>
2020-03-22 12:04:24 -0500

    Cleaned and commented main.py
    Made it more readable

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#make it here
+#Made by Jada Ebong with assistance from Leon Yuan
 import random, os
 from fpdf import FPDF
 count = 0
-#dictOfBoards = dict.fromkeys(range(5),[])
 while count!=5:
     #all possible cards
     cards = ["El gallo","El diablito","La dama","El catrín",
@@ -18,37 +17,31 @@ while count!=5:
             "La chalupa","El pino","El pescado","La palma","La maceta",
             "El arpa","La rana"]
             #change? : el negrito(Rowdy), el apache(Roadrunner), el borracho?
-
-    #slot = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]s
-    #Two options: Randomize and check all other card combos if it already happened 
-    #OR go through in order until 400 but everyones first couple will be same - this wont work bc everyone could get same bingo
-
+    
+    #slot represents the 16 different locations on a 4x4 loteria board
     slot = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+    #puts a random card into a slot, then deletes it from card pool so it can't be repeated
     for x in slot:
         newS = random.choice(cards)
         cards.remove(newS)
-        #for y in cards:
-        #    if newS == y:
-        #        cards.remove(newS)
         slot[x] = newS
+
+    #File I/O: creates a unique file for each board in RH_Loteria/boards
     filename = str(count) + '.txt'
     path = str(os.getcwd()) + "/boards/"
-    line = str.encode(str(slot))
-    print(path)
     if not os.path.exists(path):
         os.makedirs(path)
     fd = open(os.path.join(path, filename), "w+")
-    #with fd as temp_file:
     fd.write(str(slot))
-        #os.write(fd,"line")
+
     print(filename)
     print(slot)
-    
-    #dictOfBoards[count].update(slot)
-    #dictOfBoards[count].append('*********************')
+
+    #increments count for the next board
     count = count +1
 
-#print(str(dictOfBoards))
+
 #static folder holding all images, find image in folder
 #image files have to be consistant, all certain size
 #python libraries for dropping pics onto other pics
