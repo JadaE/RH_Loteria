3cd9e58f35dab6cc2731f76ec2760fbf82abc851
Jada Ebong <jadaebong@gmail.com>
2022-03-27 13:18:49 -0500

    rh22 you know

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fpdf import FPDF
 import hashlib
 count = 0
 boards = []
-while count!=500:
+while count<=200:
     #all possible cards
     cards = ["El_gallo","El_diablito","La_dama","El_catrín",
             "El_paraguas","La_sirena","La_escalera","La_botella",
@@ -14,7 +14,7 @@ while count!=500:
             "La_luna","El_cotorro","El_Rowdy","El_corazón",
             "La_sandía","El_tambor","El_camarón","Las_jaras","El_músico",
             "La_araña","El_soldado","La_estrella","El_cazo","El_mundo",
-            "El_Roadrunner","El_nopal","El_alacrán","La_rosa","La_calavera",
+            "El_nopal","El_alacrán","La_rosa","La_calavera",
             "La_campana","El_cantarito","El_venado","El_Sol","La_corona",
             "La_chalupa","El_pino","El_pescado","La_palma","La_maceta",
             "El_arpa","La_rana"]
@@ -50,7 +50,7 @@ while count!=500:
         pdf.set_font('Arial','',12)
         #pdf.set_fill_color(203, 235, 232)
         pdf.set_margins(2,1,2)
-        pdf.image("LoteriaBoardHeader_1.jpg", x = None, y = None, w = 19, h = 6, type = 'jpg', link = '')
+        pdf.image("RowdyHacks_Loteria.png", x = None, y = None, w = 19, h = 6, type = 'png', link = '')
         pdf.cell(20,1,"",0,1)
         xaxis=2
         yaxis=8
@@ -60,7 +60,7 @@ while count!=500:
                 pdf.cell(20,1,"",0,0)
                 pdf.image(cardImage, x = xaxis, y = yaxis, w = 3.5, h = 4, type = 'jpeg', link = '')
                 if x != 15:
-                   pdf.cell(20,1,"",0,0)  
+                   pdf.cell(1,1,"",0,2)  
                 xaxis = 2
                 yaxis = yaxis +5
             else :
